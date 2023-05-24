01db57603779e6235de88ce68e15b4ade3021bc3
GaraTox <garcia.cyril@outlook.com>
2023-05-24 15:33:43 +0200

    ADD function button

---
+++ script.js
@@ -0,0 +1,72 @@
+let rotat = document.getElementById("id_rotat")
+let scientific = document.getElementById("id_Calculator_Scientific")
+let container = document.getElementById("id_container")
+let simple = document.getElementById("id_Calculator_simple")
+
+rotat.addEventListener("click", Event => {
+
+    if (scientific.style.display != "none") {
+        scientific.style.display = "none"
+        simple.style.width = "100%"
+        simple.style.marginRight = "15px"
+        container.style.width = "25%"
+        container.style.height = "400px"
+    }else{
+        scientific.style.display = "block"
+        simple.style.width = "55%"
+        container.style.width = "50%"
+        container.style.height = "370px"
+    }
+})
+
+// DISPLAY
+function display(val){
+document.getElementById('id_input_write').value += val;
+return val;
+}
+
+// PARENTHESIS
+var i = 0;
+function parenthesis(){
+    let z = document.getElementById('id_input_write');
+    if(i == 0){
+        z.value += '(';
+
+        i = 1;
+    }else{
+        z.value += ')';
+        i = 0;
+    }
+}
+
+// EQUAL
+function equal(){
+    let x = document.getElementById('id_input_write').value;
+    let y = eval(x); // envoie une valeur d'achevement
+    document.getElementById('id_input_result').value = y;
+    return y;
+}
+
+// DELETE
+function clean(){
+    document.getElementById('id_input_write').value = '';
+    document.getElementById('id_input_result').value = '';
+}
+
+// SPACE
+function Back(){
+    let space = document.getElementById('id_input_write');
+    let resultat = space.value;
+    if(resultat.length > 0){
+        resultat = resultat.substring(0, resultat.length-1); // BACK TO THE LAST CHARACTER
+        space.value = resultat;
+    }
+}
+
+// OPERATOR + / -
+function addOperator(){
+    let a = document.getElementById('id_input_write');
+    let n = a.value;
+    n = n * -1;
+    a.value = n;
+ };
