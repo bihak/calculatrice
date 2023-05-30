d1b5b78f343c66a186cc92abf6980ee74efb58b2
bihak <belamiri113@gmail.com>
2023-05-30 16:07:41 +0200

    ADD js.

--- script.js
+++ script.js
@@ -31,85 +31,10 @@ function clean(){
     document.getElementById('id_input_write').value = '';
     document.getElementById('id_input_result').value = '';
 }
-    // EQUAL
-function equal(){
-    let x = document.getElementById('id_input_write').value;
-    let y = eval(x); // envoie une valeur d'achevement
-    document.getElementById('id_input_result').value = y;
-    return y;
-}
-    // FACTORIELLE
-function fact(){
-    let i = '';
-    let r = document.getElementById('id_input_write').value;
-    f = 1;
-    for(i = 1; i <= r; i++){
-        f = f * i;
-    }
-    document.getElementById('id_input_result').value = f;
-}
-    // RACINE CARRE
-function square_root(chiffre) {
-    let y = Math.sqrt(chiffre)
-        return y
-}
-    // COS
-function cos(chiffre) {
-    let y = Math.cos(chiffre)
-    return y
-}
-    // SIN
-    function sin(chiffre) {
-        let y = Math.sin(chiffre)
-        return y
-    }
-    // TAN
-function tan(chiffre) {
-        let y = Math.tan(chiffre)
-        return y
-}
-// LOGARITHME NEPERIEN
-function ln(chiffre){
-    let y = Math.log(chiffre);
-    return y
-}
-
-    // LOG
-function log(chiffre){
-    let y = (Math.log(chiffre))/(Math.log(10));
-    return y
-}
-
-    // INVERSE
-function inverse(chiffre){
-    let y = (1/chiffre);
-    return y
-}
-    // EXP
-function exp(chiffre) {
-    let y = Math.exp(chiffre)
-        return y
-    }
-    // ABSOLUTE
-function absolute(chiffre) {
-    let y = Math.abs(chiffre)
-    return y
-}
-    // CARRE
-function carre(chiffre) {
-    let y = (chiffre * chiffre)
-    return y
-    }
-    // POWER
-function power(chiffre , exposant) {
-    let y = Math.pow(chiffre, exposant);
-    return y
-    }
 // FACTORIELLE
-function fact(chiffre){
-    let i = '';
+function fac(chiffre){
     y = 1;
-    for(i = 1; i <= chiffre; i++){
+    for(let i = 1; i <= chiffre; i++){
         y = y * i;
     }
     return y
@@ -127,25 +52,18 @@ function parenthesis(){
     }
 }
 
-
-
-// EQUAL
-function equal(tabnomber,taboperateur){
-    let opresult = []
-    let j = 0
-    for(let i=0 ; i<tabnomber.length ; i++){
-        opresult [j] = tabnomber [i]
-        if ((i+1)!=tabnomber.length) {
-            opresult[j+1] = taboperateur[i]
-            j = j + 2
-        }  
-    }
-    let x = opresult.join("")
-    let y = eval(x);
-    if (x.includes("/0")) {
+function equal(chaine){
+    let y = eval(chaine);
+    if (chaine.includes("/0")) {
         y = "Erreur : Division par zéro";
     }
     document.getElementById('id_input_result').value = y;
+    return y;
+}
+// POURCENTAGE
+function pourcentage(chiffre) {
+    let y
+    y = chiffre / 100
     return y;
 }
 
@@ -169,83 +87,14 @@ function Back(){
 function recup() {
     let input_write = document.getElementById('id_input_write');
     let resultat = input_write.value
-    let taboperateur = resultat.match(/[+\-*\/](?![^(]*\))/g)
-    let tabnomber = resultat
-    .replace(/(\d+(?:\.\d+)?)-/g, "$1,-") 
-    .match(/(?:\b\w+\(-?\d+(?:\.\d+)?\))|(?:-?\d+(?:\.\d+)?(?:²|√)?)/g)
+    let chaine = resultat
+    const fonctions = ["sqrt", "sin","asin","acos","atan", "cos", "tan", "log", "exp", "cbrt", "pow", "abs"];
 
-    for (let k = 0 ; k < tabnomber.length ; k++){
-        if (tabnomber[k].includes("Fac")||tabnomber[k].includes("Sqrt")||tabnomber[k].includes("sin")||
-            tabnomber[k].includes("cos")||tabnomber[k].includes("tan")||tabnomber[k].includes("Ln")||
-            tabnomber[k].includes("Log")||tabnomber[k].includes("Inv")||tabnomber[k].includes("exp")||
-            tabnomber[k].includes("²")||tabnomber[k].includes("Pow")||tabnomber[k].includes("Abs")){
-            if (tabnomber[k].includes("Fac")) {
-                    let nombre = tabnomber[k]
-                    let chiffre = nombre.match(/\d+/);
-                    tabnomber[k]=fact(chiffre)
-                }
-                else if(tabnomber[k].includes("Sqrt")) {
-                    let nombre = tabnomber[k]
-                    let chiffre = nombre.match(/\d+/);
-                    tabnomber[k]=square_root(chiffre)
-                }
-                else if(tabnomber[k].includes("sin")) {
-                    let nombre = tabnomber[k]
-                    let chiffre = nombre.match(/\d+/);
-                    tabnomber[k]=sin(chiffre)
-                }
-                else if(tabnomber[k].includes("cos")) {
-                    let nombre = tabnomber[k]
-                    console.log(nombre)
-                    let chiffre = nombre.match(/\d+/);
-                    tabnomber[k]=cos(chiffre)
-                }
-                else if(tabnomber[k].includes("tan")) {
-                    let nombre = tabnomber[k]
-                    let chiffre = nombre.match(/\d+/);
-                    tabnomber[k]=tan(chiffre)
-                }
-                else if(tabnomber[k].includes("Ln")) {
-                    let nombre = tabnomber[k]
-                    let chiffre = nombre.match(/\d+/);
-                    tabnomber[k]=ln(chiffre)
-                }
-                else if(tabnomber[k].includes("Log")) {
-                    let nombre = tabnomber[k]
-                    let chiffre = nombre.match(/\d+/);
-                    tabnomber[k]=log(chiffre)
-                }
-                else if(tabnomber[k].includes("Inv")) {
-                    let nombre = tabnomber[k]
-                    let chiffre = nombre.match(/\d+/);
-                    tabnomber[k]=inverse(chiffre)
-                }
-                else if(tabnomber[k].includes("exp")) {
-                    let nombre = tabnomber[k]
-                    let chiffre = nombre.match(/\d+/);
-                    tabnomber[k]=exp(chiffre)
-                }
-                else if(tabnomber[k].includes("²")) {
-                    let nombre = tabnomber[k]
-                    let chiffre = nombre.match(/\d+/);
-                    tabnomber[k]=carre(chiffre)
-                }
-                else if(tabnomber[k].includes("Pow")) {
-
-                    let nombre = tabnomber[k]
-                    let matches = nombre.match(/\((.*?)\)/);
-                    let chiffre = matches[1][0]
-                    let exposant = matches[1][2]
-                    tabnomber[k] = power(chiffre,exposant)
-                }
-                else if(tabnomber[k].includes("Abs")) {
-                    let nombre = tabnomber[k]
-                    let chiffre = nombre.match(/\d+/);
-                    tabnomber[k]=absolute(chiffre)
-                }
-    }
-    }
-    equal(tabnomber,taboperateur)
+    fonctions.forEach(fonction => {
+        const regex = new RegExp('\\b' + fonction + '\\b', 'g');
+        chaine = chaine.replace(regex, 'Math.' + fonction);
+    });
+    equal(chaine)
 }
     // OPERATOR + / -
 function addOperator(){
@@ -254,4 +103,3 @@ function addOperator(){
     n = n * -1;
     a.value = n;
 };
-
